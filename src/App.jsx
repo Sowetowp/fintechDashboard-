be222edc40bf60751d78b6589db0e997366d6d77
Abdulmalik <ayodejiamzat@gmail.com>
2024-06-04 22:44:13 +0100

    auto-commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 
 import './App.css'
+import Dashboard from './Pages/Dashboard'
 
 function App() {
 
@@ -9,7 +10,7 @@ function App() {
     <>
       <Router>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Dashboard />} />
         </Routes>
       </Router>
     </>
