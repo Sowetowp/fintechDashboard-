3c6e9f97103adc0298e2b45298809beaa82737c0
Abdulmalik <ayodejiamzat@gmail.com>
2024-06-04 23:03:37 +0100

    auto-commit

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -5,7 +5,9 @@ const Navbar = () => {
     <>
         <header className='w-screen sticky top-0'>
             <section className='max-w-7xl bg-blue-400 h-3 mx-auto'>
-                <div className='w-1/2 bg-green-300'></div>
+                <div className='w-1/2 bg-green-300'>
+                    
+                </div>
             </section>
         </header>
     </>
