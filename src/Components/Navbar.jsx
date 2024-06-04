fd93be647118e6d9942c2b0289b2959c72f8d152
Abdulmalik <ayodejiamzat@gmail.com>
2024-06-04 23:54:49 +0100

    auto-commit

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import logo from "../assets/logo.png"
 import logo2 from "../assets/logo-text.png"
+import search from "../assets/logo-text.png"
 import "../Styles/Dashboard.css"
 
 const Navbar = () => {
