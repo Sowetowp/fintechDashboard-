55a4301e4cbecbf02446e1904be453e37748451d
Abdulmalik <ayodejiamzat@gmail.com>
2024-06-04 22:43:30 +0100

    auto-commit

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,20 @@ function App() {
 
   return (
     <>
-      
+      <Router>
+        <ErrorBoundary>
+          <Suspense fallback={<div id='preloader-active'>
+            <div className='preloader d-flex align-items-center justify-content-center'>
+              <div className='preloader-inner position-relative'>
+                <div className="superballs">
+                  <div className="superballs__dot"></div>
+                  <div className="superballs__dot"></div>
+                </div>
+              </div>
+            </div>
+          </div>}>
+            <Routes>
+              <Route path="/" element={<Home />} />
     </>
   )
 }
