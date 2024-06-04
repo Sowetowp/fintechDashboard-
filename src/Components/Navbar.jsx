0968eefeb408a01ab300f1aa832569e286635c7a
Abdulmalik <ayodejiamzat@gmail.com>
2024-06-04 23:58:59 +0100

    auto-commit

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -25,7 +25,7 @@ const Navbar = () => {
                             </a>
                             <div className='flex bg-slate-200'>
                                 <img src={search} className="w-6" alt="" />
-                                <input className='bg-transparent border' type="text" placeholder='Search here'/>
+                                <input className='bg-transparent' type="text" placeholder='Search here'/>
                             </div>
                         </div>
                     </div>
