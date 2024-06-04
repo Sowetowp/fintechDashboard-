9096d254995f26262609f7dabe462766446de1e2
Abdulmalik <ayodejiamzat@gmail.com>
2024-06-04 22:47:28 +0100

    auto-commit

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Navbar = () => {
   return (
     <>
-        <main className='max-w-xl bg-slate-400 h-2'></main>
+        <main className='max-w-2xl bg-slate-400 h-2'></main>
     </>
   )
 }
