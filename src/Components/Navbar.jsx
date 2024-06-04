def9fa1ccdca403ed46a8bcae81efa67ddb50a16
Abdulmalik <ayodejiamzat@gmail.com>
2024-06-04 23:07:24 +0100

    auto-commit

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = () => {
                 <div className='w-1/2 bg-green-300'>
                     <a href="flex flex-row items-center">
                         <img src={logo} alt="logo" />
-                        <img src="" alt="logo" />
+                        <img src={logo2} alt="logo" />
                     </a>
                 </div>
             </section>
