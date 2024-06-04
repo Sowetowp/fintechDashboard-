4839121c064d6858dd47f73d77b0695845d222b5
Abdulmalik <ayodejiamzat@gmail.com>
2024-06-04 23:01:09 +0100

    auto-commit

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 const Navbar = () => {
   return (
     <>
-        <header className='w-screen '>
+        <header className='w-screen sticky'>
             <section className='max-w-7xl bg-blue-400 h-3 mx-auto'>
-                
+
             </section>
         </header>
     </>
