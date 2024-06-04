bceab597c6f9d67e2736ca27c2aa15f1dd7026ae
Abdulmalik <ayodejiamzat@gmail.com>
2024-06-04 23:57:35 +0100

    auto-commit

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -23,7 +23,7 @@ const Navbar = () => {
                                     <span></span>
                                 </label>
                             </a>
-                            <div className='flex'>
+                            <div className='flex bg'>
                                 <img src={search} className="w-6" alt="" />
                                 <input type="text" placeholder='Search here'/>
                             </div>
