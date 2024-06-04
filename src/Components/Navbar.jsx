2a5b0f4dd48abb153f0d627ea87c096aca44fe27
Abdulmalik <ayodejiamzat@gmail.com>
2024-06-04 23:13:59 +0100

    auto-commit

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = () => {
                 <div className='w-1/2 bg-green-300'>
                     <a className="pl-5 flex gap-3 flex-row items-center">
                         <img className='w-12' src={logo} alt="logo" />
-                        <img className='w-24' src={logo2} alt="logo" />
+                        <img className='w-28' src={logo2} alt="logo" />
                     </a>
                 </div>
             </section>
