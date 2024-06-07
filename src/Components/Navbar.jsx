c27c11907561536525b92124b1412b845599f3d9
Abdulmalik <ayodejiamzat@gmail.com>
2024-06-07 19:44:56 +0100

    auto-commit

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -36,7 +36,7 @@ const Navbar = () => {
                         </div>
                     </div>
                     <div className="w-1/2 flex justify-around items-cente">
-                        <div className='bg-slate-300 m-0 flex items-center rounded-full'>
+                        <div className='bg-slate-300 flex items-center rounded-full'>
                             <div className='bg-[#0099FF] flex items-center gap-3 py-1 rounded-full px-3'>
                                 <img className='w-7' src={cloud} alt="" />
                                 <span className='text-xl font-medium text-white'>21</span>
