a71884a7dbf50d28608fdf2d8e240bd744642601
Abdulmalik <ayodejiamzat@gmail.com>
2024-06-08 00:11:30 +0100

    auto-commit

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -25,7 +25,7 @@ const Navbar = () => {
     return (
         <>
             <header className='w-screen sticky top-0 dark:bg-blue-950 '>
-                {/* <section className='max-w-7xl w-full bg-slate-500 mx-auto py-5 flex items-center'> */}
+                <section className='max-w-7xl w-full bg-slate-500 mx-auto py-5 flex items-center'>
                     <div className='w-1/2 flex items-center justify-between'>
                         <a className="pl-5 flex gap-3 flex-row items-center">
                             <img className='w-14' src={logo} alt="logo" />
@@ -71,7 +71,7 @@ const Navbar = () => {
                         </a>
                         <img src={dp} className='rounded-[50%] w-10' alt="" />
                     </div>
-                {/* </section> */}
+                </section>
             </header>
         </>
     )
