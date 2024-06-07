3f7539c8954ff6170b04157c296a164f77bc13e3
Abdulmalik <ayodejiamzat@gmail.com>
2024-06-07 21:24:46 +0100

    auto-commit

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -12,15 +12,15 @@ import "../Styles/Dashboard.css"
 
 const Navbar = () => {
     const [dark, setDark] = useState(false)
-    const darkMode = ()=>{
-            if (dark) {
-              localStorage.setItem('theme', 'light');
-            } else {
-              html.classList.add('dark');
-              localStorage.setItem('theme', 'dark');
-            }
-          
+    const darkMode = () => {
+        if (dark) {
+            localStorage.setItem('theme', 'light');
+        } else {
+            localStorage.setItem('theme', 'dark');
+        }
+
     }
+    
     return (
         <>
             <header className='w-screen sticky top-0 dark:bg-blue-950 light'>
@@ -41,7 +41,7 @@ const Navbar = () => {
                             </a>
                             <div className='flex bg-slate-200 rounded-lg px-6 items-center gap-3 py-2'>
                                 <img src={search} className="w-6" alt="" />
-                                <input className='bg-transparent w-68 font-light outline-0' type="text" placeholder='Search here...'/>
+                                <input className='bg-transparent w-68 font-light outline-0' type="text" placeholder='Search here...' />
                             </div>
                         </div>
                     </div>
