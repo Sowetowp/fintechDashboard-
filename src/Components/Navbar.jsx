2be6516e4ddc468e4d87357f889adb239ba372fa
Abdulmalik <ayodejiamzat@gmail.com>
2024-06-07 19:28:00 +0100

    auto-commit

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -6,7 +6,7 @@ import cloud from "../assets/cloud-svgrepo-com.svg"
 import moon from "../assets/night-moon-svgrepo-com.svg"
 import day from "../assets/day-festival-light-2-svgrepo-com.svg"
 import bell from "../assets/bell-svgrepo-com.svg"
-import bell from "../assets/chat-line-square-svgrepo-com.svg"
+import chat from "../assets/chat-line-square-svgrepo-com.svg"
 import "../Styles/Dashboard.css"
 
 const Navbar = () => {
