45bbb1e95721d69993ba1ab6313d5b6b8c2b758f
Abdulmalik <ayodejiamzat@gmail.com>
2024-06-07 19:41:12 +0100

    auto-commit

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -58,7 +58,7 @@ const Navbar = () => {
                             <span className='leading-tight'>Hello,<b>Franklin</b></span>
                             <span className='text-sm text-gray-500'>super admin</span>
                         </a>
-                        <img src="" alt="" />
+                        <img src={dp} alt="" />
                     </div>
                 </section>
             </header>
