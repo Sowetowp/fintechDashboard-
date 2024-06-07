b3292dffba1b92252ff5e0e88444fe4df24f655c
Abdulmalik <ayodejiamzat@gmail.com>
2024-06-07 21:22:45 +0100

    auto-commit

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -12,7 +12,16 @@ import "../Styles/Dashboard.css"
 
 const Navbar = () => {
     const darkMode = ()=>{
-       
+        function toggleDarkMode() {
+            const html = document.documentElement;
+            if (html.classList.contains('dark')) {
+              html.classList.remove('dark');
+              localStorage.setItem('theme', 'light');
+            } else {
+              html.classList.add('dark');
+              localStorage.setItem('theme', 'dark');
+            }
+          }
     }
     return (
         <>
