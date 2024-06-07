2e23b03c23c09d15cecf181e344629ed7fb7038c
Abdulmalik <ayodejiamzat@gmail.com>
2024-06-07 22:31:34 +0100

    auto-commit

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -43,7 +43,7 @@ const Navbar = () => {
                                     <span></span>
                                 </label>
                             </a>
-                            <div className='flex bg-slate-200 rounded-lg px-6 items-center gap-3 py-2'>
+                            <div className='flex bg-slate-200 rounded-lg px-6 items-center gap-3 py-2 dark:bg-[rgb(23,37,84)]'>
                                 <img src={search} className="w-6" alt="" />
                                 <input className='bg-transparent w-68 font-light outline-0' type="text" placeholder='Search here...' />
                             </div>
