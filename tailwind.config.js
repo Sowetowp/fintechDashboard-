1a8126e6fd85df3e61a199d5a364e0fc5a7d70d6
Abdulmalik <ayodejiamzat@gmail.com>
2024-06-08 00:57:09 +0100

    auto-commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'xs': '480px',  // Custom breakpoint for extra small screens
+      },
+    },
   },
   darkMode: ['variant', [
     '@media (prefers-color-scheme: dark) { &:not(.light *) }',
