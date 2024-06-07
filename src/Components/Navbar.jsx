72f75cfc566e85713e5670f84bcc7cf3f89f14fd
Abdulmalik <ayodejiamzat@gmail.com>
2024-06-07 21:47:28 +0100

    auto-commit

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -13,7 +13,7 @@ import "../Styles/Dashboard.css"
 const Navbar = () => {
     const [dark, setDark] = useState(false)
     const darkMode = () => {
-        document.body.classList.toggle("light");
+        document.body.classList.add("light");
         localStorage.setItem('theme', `${dark ? "dark" : "light"}`);
     }
 
