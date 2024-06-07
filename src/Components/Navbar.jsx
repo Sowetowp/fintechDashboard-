01b7c7562e5f9ef50c18d9f81d1360630b22e2fe
Abdulmalik <ayodejiamzat@gmail.com>
2024-06-07 18:09:32 +0100

    auto-commit

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -31,7 +31,7 @@ const Navbar = () => {
                     </div>
                     <div className="w-1/2 flex item-center">
                         <div>
-                            <div className='bg-blue-500'>
+                            <div className='bg-cyan-500'>
                                 <img src="" alt="" />
                                 <span>21</span>
                             </div>
