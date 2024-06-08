de191e985bc0eff7155f13d50cde7e659cc81d64
Abdulmalik <ayodejiamzat@gmail.com>
2024-06-08 03:10:58 +0100

    auto-commit

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -75,9 +75,9 @@ const Navbar = () => {
                                 <span className='text-sm text-gray-500 dark:text-white'>super admin</span>
                             </a>
                             <div className="bg-white rounded-md w-[max-content] absolute py-3">
-                                <a className='flex gap-4 px-5 py-2 hover:bg-gray-100'><img className='w-4' src={profile} alt="" />Profile</a>
-                                <a className='flex gap-4 px-5 py-2 hover:bg-gray-100'><img className='w-4' src={inbox} alt="" />Inbox</a>
-                                <a className='flex gap-4 px-5 py-2 hover:bg-gray-100'><img className='w-4' src={logout} alt="" />Logout</a>
+                                <a className='flex gap-4 px-5 py-2 hover:bg-gray-100'><img className='w-6' src={profile} alt="" />Profile</a>
+                                <a className='flex gap-4 px-5 py-2 hover:bg-gray-100'><img className='w-6' src={inbox} alt="" />Inbox</a>
+                                <a className='flex gap-4 px-5 py-2 hover:bg-gray-100'><img className='w-6' src={logout} alt="" />Logout</a>
                             </div>
                         </div>
                         <img src={dp} className='rounded-[50%] w-10' alt="" />
