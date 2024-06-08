8aa88adce214cf84268fcbafc88c2aee879e37a4
Abdulmalik <ayodejiamzat@gmail.com>
2024-06-08 02:51:05 +0100

    auto-commit

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -75,7 +75,7 @@ const Navbar = () => {
                             <div className="bg-red-500 min-w-full absolute">
                                 <ul>
                                     <li>
-                                        <a href=""></a><img src={profile} alt="" />Profile
+                                        <a href=""><img src={profile} alt="" />Profile
                                     </li>
                                 </ul>
                             </div>
