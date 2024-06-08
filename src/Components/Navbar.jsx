0179dfbbdf014ce7131df811caf7f9ad00bf9e5c
Abdulmalik <ayodejiamzat@gmail.com>
2024-06-08 03:02:51 +0100

    auto-commit

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -7,7 +7,7 @@ import day from "../assets/day-festival-light-2-svgrepo-com.svg"
 import dp from "../assets/17.jpg"
 import profile from "../assets/profile-svgrepo-com (2).svg"
 import inbox from "../assets/profile-svgrepo-com (2).svg"
-import profile from "../assets/profile-svgrepo-com (2).svg"
+import logout from "../assets/profile-svgrepo-com (2).svg"
 import "../Styles/Dashboard.css"
 
 const Navbar = () => {
