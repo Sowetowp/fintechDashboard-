a39d5db793e176ced0ead696293049b8b30d082a
Abdulmalik <ayodejiamzat@gmail.com>
2024-06-09 18:53:24 +0100

    auto-commit

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -76,7 +76,7 @@ const Navbar = () => {
                                 <span className='leading-tight'>Hello,<b>Franklin</b></span>
                                 <span className='text-sm text-gray-500 dark:text-white'>super admin</span>
                             </a>
-                            <a href="" className='bg-blue-500 absolute w-screen h-screen top-0 left-0 right-0'></a>
+                            <a href="" className='bg-blue-500 relative w-screen h-screen top-0 left-0 right-0'></a>
                             <div className={`bg-white shadow mt-7 m-2 min-w-40 rounded-md w-[max-content] absolute py-3 ${profile ? "block" : "hidden"}`}>
                                 <a className='flex gap-4 px-5 py-2 hover:bg-gray-100'><img className='w-5' src={profileico} alt="" />Profile</a>
                                 <a className='flex gap-4 px-5 py-2 hover:bg-gray-100'><img className='w-5' src={inbox} alt="" />Inbox</a>
