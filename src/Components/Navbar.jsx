533406dfc24a59266a410e6b67e6262fee02a135
Abdulmalik <ayodejiamzat@gmail.com>
2024-06-09 21:19:36 +0100

    auto-commit

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -101,7 +101,7 @@ const Navbar = () => {
                                     <a key={index} className='flex items-center p-3 border-b gap-3'>
                                         <img src={noti.image} alt="" className='w-14 h-14 rounded-lg' />
                                         <span>
-                                            <p style={{ wordBreak: "break-word" }} className='font-semibold text-md text-gray-700'>Dr sultads Send you Photo</p>
+                                            <p style={{ wordBreak: "break-word" }} className='font-semibold text-md text-gray-700'>{noti.title}</p>
                                             <p style={{ wordBreak: "break-word" }}>29 July 2020 - 02:26 PM</p>
                                         </span>
                                     </a>
