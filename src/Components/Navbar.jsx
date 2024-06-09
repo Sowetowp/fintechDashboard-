90e431286ddbff2a19d141caeac7c06b7bc15803
Abdulmalik <ayodejiamzat@gmail.com>
2024-06-09 18:57:52 +0100

    auto-commit

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -71,7 +71,7 @@ const Navbar = () => {
                             <svg className='w-4' viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg"><path fill={dark ? "white" : "#000000"} d="M160 826.88 273.536 736H800a64 64 0 0 0 64-64V256a64 64 0 0 0-64-64H224a64 64 0 0 0-64 64v570.88zM296 800 147.968 918.4A32 32 0 0 1 96 893.44V256a128 128 0 0 1 128-128h576a128 128 0 0 1 128 128v416a128 128 0 0 1-128 128H296z" /><path fill={dark ? "white" : "#000000"} d="M352 512h320q32 0 32 32t-32 32H352q-32 0-32-32t32-32zm0-192h320q32 0 32 32t-32 32H352q-32 0-32-32t32-32z" /></svg>
                             <sup className='absolute'><span className='px-[4px] ml-6 text-xs text-white bg-[#0099FF] rounded-full'>5</span></sup>
                         </div>
-                        {profile && <a className='bg-[#80808041] absolute w-screen h-screen top-0 left-0 right-0 z-10'></a>}
+                        {profile && <a onClick={()=>setProfile(!profile)} className='bg-[#80808041] absolute w-screen h-screen top-0 left-0 right-0 z-10'></a>}
                         <div className='relative hidden sm:inline-block'>
                             <a onClick={()=>setProfile(!profile)} className='text-right flex-col dark:text-white flex'>
                                 <span className='leading-tight'>Hello,<b>Franklin</b></span>
