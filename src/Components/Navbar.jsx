eee9745f9fa5e7fe541985bff00ee6c9740896f1
Abdulmalik <ayodejiamzat@gmail.com>
2024-06-10 00:46:02 +0100

    auto-commit

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -46,7 +46,7 @@ const Navbar = () => {
             notifRef.current.focus();
         }
     };
-    
+
     const notifs = [
         {
             image: notif1,
@@ -126,7 +126,7 @@ const Navbar = () => {
                             <img className='w-4' src={dark ? day : moon} alt="" />
                         </a>
                         <div className=''>
-                            <a onClick={() => { setNotification(!notification); notifRef.current.focus(); }} className="bg-white shadow relative cursor-pointer rounded-[50%] h-[fit-content] p-2 flex items-center justify-center dark:bg-[rgb(17,26,56)]">
+                            <a onClick={toggleNotification} className="bg-white shadow relative cursor-pointer rounded-[50%] h-[fit-content] p-2 flex items-center justify-center dark:bg-[rgb(17,26,56)]">
                                 <svg className='w-4' viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg"><path fill={dark ? "white" : "#000000"} d="M512 64a64 64 0 0 1 64 64v64H448v-64a64 64 0 0 1 64-64z" /><path fill={dark ? "white" : "#000000"} d="M256 768h512V448a256 256 0 1 0-512 0v320zm256-640a320 320 0 0 1 320 320v384H192V448a320 320 0 0 1 320-320z" /><path fill={dark ? "white" : "#000000"} d="M96 768h832q32 0 32 32t-32 32H96q-32 0-32-32t32-32zm352 128h128a64 64 0 0 1-128 0z" /></svg>
                                 <sup className='absolute'><span className='px-[4px] ml-6 text-xs text-white bg-[#0099FF] rounded-full'>12</span></sup>
                             </a>
