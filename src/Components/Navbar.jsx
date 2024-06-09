c5ea97aa70743a3c73bb0b44fe1d3b560f66eb0d
Abdulmalik <ayodejiamzat@gmail.com>
2024-06-09 23:49:58 +0100

    auto-commit

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -39,6 +39,7 @@ const Navbar = () => {
 
     useEffect(()=>{
         const systemPrefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+        alert()
     },[])
     const notifs = [
         {
