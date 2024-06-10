89c3ba267daa72c30c978567034110eb92db2087
Abdulmalik <ayodejiamzat@gmail.com>
2024-06-10 20:43:45 +0100

    auto-commit

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -165,7 +165,7 @@ const Navbar = () => {
                         <a onClick={() => setProfile(!profile)}><img src={dp} className='rounded-[50%] cursor-pointer w-10' alt="" /></a>
                     </div>
                 </section>
-                {(profile || notification || chat) && <a onClick={() => { setProfile(false); setNotification(false) }} className='bg-[#80808024] absolute w-full h-screen top-0 left-0 right-0 z-10'></a>}
+                {(profile || notification || chat) && <a onClick={() => { setProfile(false); setNotification(false); setChat(false) }} className='bg-[#80808024] absolute w-full h-screen top-0 left-0 right-0 z-10'></a>}
                 {chat && <section className='absolute right-0 h-screen top-0 bg-red-500 z-40'>
                     <div className='flex justify-around w-full md:w-[22rem] bg-[#0099FF] pt-5 '>
                         <a onClick={()=>{setAlerts(false); setChats(false); setHome(true)}} className={`text-white text-base font-semibold pb-3 ${home ? "border-b-2" : "" }`}>HOME</a>
