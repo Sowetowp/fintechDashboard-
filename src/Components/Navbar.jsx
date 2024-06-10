5aee120f46c06ed7d6927dee13924d7c11de9dfb
Abdulmalik <ayodejiamzat@gmail.com>
2024-06-10 10:57:48 +0100

    auto-commit

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -165,7 +165,9 @@ const Navbar = () => {
                 {(profile || notification) && <a onClick={() => { setProfile(false); setNotification(false) }} className='bg-[#80808024] absolute w-full h-screen top-0 left-0 right-0 z-10'></a>}
                 <section className='absolute right-0 h-screen top-0 bg-red-500 z-40'>
                     <main className='w-full md:w-80'>
-                        <div></div>
+                        <div>
+                            <a href=""></a>
+                        </div>
                     </main>
                 </section>
             </header>
