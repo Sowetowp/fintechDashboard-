c9f5dd1676e61106d7bc3f37c80ff4f628780841
Abdulmalik <ayodejiamzat@gmail.com>
2024-06-10 20:29:55 +0100

    auto-commit

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -168,7 +168,7 @@ const Navbar = () => {
                 {(profile || notification) && <a onClick={() => { setProfile(false); setNotification(false) }} className='bg-[#80808024] absolute w-full h-screen top-0 left-0 right-0 z-10'></a>}
                 <section className='absolute right-0 h-screen top-0 bg-red-500 z-40'>
                     <div className='flex justify-around w-full md:w-[22rem] bg-[#0099FF] pt-5 '>
-                        <a href="" className='text-white text-base font-semibold border-b-2 pb-3'>HOME</a>
+                        <a href="" className={'text-white text-base font-semibold border-b-2 pb-3'}>HOME</a>
                         <a href="" className='text-white text-base font-semibold border-b-2 pb-3'>ALERTS</a>
                         <a href="" className='text-white text-base font-semibold border-b-2 pb-3'>CHAT</a>
                     </div>
