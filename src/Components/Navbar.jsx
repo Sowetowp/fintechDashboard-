acf4509d0f82afe92d835b0668ec15a63c46724f
Abdulmalik <ayodejiamzat@gmail.com>
2024-06-11 00:11:04 +0100

    auto-commit

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -288,7 +288,7 @@ const Navbar = () => {
                     </div>
                 </section>
                 {(profile || notification || chat) && <a onClick={() => { setProfile(false); setNotification(false); setChat(false) }} className='bg-[#80808024] absolute w-full h-screen top-0 left-0 right-0 z-10'></a>}
-                <section className={`fixed right-0 h-screen top-0 z-40 w-full md:w-[22rem]`} style={{ transform: chat ? "translateX(0%)" : "translateX(100%)", transition: "transform .7s ease" }}>
+                <section className={`fixed right-0 h-screen top-0 z-40 w-[90%] md:w-[22rem]`} style={{ transform: chat ? "translateX(0%)" : "translateX(100%)", transition: "transform .7s ease" }}>
                     <div className='flex justify-around bg-[#0099FF] pt-5 '>
                         <a onClick={() => { setAlerts(false); setChats(false); setHome(true) }} className={`text-white text-base cursor-pointer font-semibold pb-3 ${home ? "border-b-2" : ""}`}>HOME</a>
                         <a onClick={() => { setAlerts(true); setChats(false); setHome(false) }} className={`text-white text-base cursor-pointer font-semibold pb-3 ${alerts ? "border-b-2" : ""}`}>ALERTS</a>
