015db9981ea7c3ad722214d484e7e3fdbcee75f5
Abdulmalik <ayodejiamzat@gmail.com>
2024-06-10 22:50:47 +0100

    auto-commit

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -191,7 +191,7 @@ const Navbar = () => {
                     <div>
                         <div>
                             <p>A</p>
-                            
+                            <div></div>
                         </div>
                     </div>
                 </section>
