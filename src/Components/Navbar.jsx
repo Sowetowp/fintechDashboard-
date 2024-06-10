2b86d1de0af9309a07f391ded73f601e636bd93e
Abdulmalik <ayodejiamzat@gmail.com>
2024-06-10 01:03:08 +0100

    auto-commit

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import logo from "../assets/logo.png"
 import logo2 from "../assets/logo-text.png"
 import cloud from "../assets/cloud-svgrepo-com.svg"
