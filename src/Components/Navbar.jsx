ffed7400fa81cbc991082d1130c9e616bee3829d
Abdulmalik <ayodejiamzat@gmail.com>
2024-06-10 21:44:32 +0100

    auto-commit

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -173,7 +173,7 @@ const Navbar = () => {
                         <a onClick={()=>{setAlerts(false); setChats(true); setHome(false)}} className={`text-white text-base font-semibold pb-3 ${chats ? "border-b-2" : "" }`}>CHAT</a>
                     </div>
                     <div>
-                        <button className='j text-'>+</button>
+                        <button className='text-gray-600 bg-gray-400'>+</button>
                         <span>
                             <p>Chat List</p>
                             <p>Show All</p>
