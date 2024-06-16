c93889f06dcc0cbb19ff8a7db598dea7af0396b5
Abdulmalik <ayodejiamzat@gmail.com>
2024-06-16 08:03:05 +0100

    auto-commit

--- src/Components/DashBody.jsx
+++ src/Components/DashBody.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 
 const DashBody = () => {
-  return (
-    <>
-    <main className='w-full bg-white dark:bg-blue-950'>
-<section className='max-w-7xl w-full mx-auto'>
+    return (
+        <>
+            <main className='w-full bg-white dark:bg-blue-950'>
+                <section className='max-w-7xl w-full mx-auto'>
 
-</section>
-    </main>
-    </>
-  )
+                </section>
+            </main>
+        </>
+    )
 }
 
 export default DashBody
