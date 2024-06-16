889c40faf977d6752ab726057adf72a1cc61e2d5
Abdulmalik <ayodejiamzat@gmail.com>
2024-06-16 08:05:26 +0100

    auto-commit

--- src/Components/DashBody.jsx
+++ src/Components/DashBody.jsx
@@ -5,7 +5,7 @@ const DashBody = () => {
         <>
             <main className='w-full bg-white dark:bg-blue-950'>
                 <section className='max-w-7xl w-full mx-auto'>
-
+                    <div className='w-1/5 bg-black'>s</div>
                 </section>
             </main>
         </>
