1fcc8319057406b8e9bc2f0977514c79d646b253
Abdulmalik <ayodejiamzat@gmail.com>
2024-06-16 19:38:30 +0100

    auto-commit

--- src/Components/DashBody.jsx
+++ src/Components/DashBody.jsx
@@ -79,7 +79,7 @@ const DashBody = () => {
                         ))}
                         <p className='px-8 text-xs text-gray-500 font-bold pt-14'>Mophy Payment Admin</p>
                         <p className='px-8 text-xs text-gray-500'>Dashboard © 2023 All Rights Reserved</p>
-                        <p className='px-8 text-xs pt-3 pb-8 text-gray-500'>Made with &#10084; by DexignZone</p>
+                        <p className='px-8 text-xs pt-3 pb-8 text-gray-500'>Made with <span style={{color: "blue"}}>&#10084;</span> by DexignZone</p>
                     </div>
                 </section>
             </main>
