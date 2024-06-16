d158ebacb92d17eb06228794d1a8127e0cc0400c
Abdulmalik <ayodejiamzat@gmail.com>
2024-06-16 14:41:07 +0100

    auto-commit

--- src/Components/DashBody.jsx
+++ src/Components/DashBody.jsx
@@ -15,7 +15,7 @@ const drops = {
 const dropReducer = (state, action) => {
     switch (action.type) {
         case "dashboard":
-            return Object.keys(state).reduce((acc, cuu)={
+            return Object.keys(state).reduce((acc, cuu)=>{
                 acc[cuu]: cuu === action.payload
             }, {})
         default:
