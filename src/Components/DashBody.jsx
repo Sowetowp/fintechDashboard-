802b2d38f2b5101da91312f3d9316304d4fd5d0a
Abdulmalik <ayodejiamzat@gmail.com>
2024-06-17 15:45:49 +0100

    auto-commit

--- src/Components/DashBody.jsx
+++ src/Components/DashBody.jsx
@@ -110,7 +110,7 @@ const DashBody = (prop) => {
                                                 </div>
                                             </div>
                                         </div>
-                                        <div className='shadow roun'></div>
+                                        <div className='shadow rounded-full w-'></div>
                                         <div className='w-[80%] m-auto shadow rounded-b-xl h-8'></div>
                                         <div className='w-[60%] m-auto shadow rounded-b-xl h-8'></div>
                                     </div>
