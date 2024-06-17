f25171cda63ff8c5f0c457e76476d6a4af7494ae
Abdulmalik <ayodejiamzat@gmail.com>
2024-06-17 15:48:34 +0100

    auto-commit

--- src/Components/DashBody.jsx
+++ src/Components/DashBody.jsx
@@ -111,7 +111,7 @@ const DashBody = (prop) => {
                                             </div>
                                         </div>
                                         <div className='shadow rounded-full w-16 h-16 absolute'>
-                                            
+                                        <svg fill="#000000" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><path d="M11.178 19.569a.998.998 0 0 0 1.644 0l9-13A.999.999 0 0 0 21 5H3a1.002 1.002 0 0 0-.822 1.569l9 13z"></path></g></svg>
                                         </div>
                                         <div className='w-[80%] m-auto shadow rounded-b-xl h-8'></div>
                                         <div className='w-[60%] m-auto shadow rounded-b-xl h-8'></div>
