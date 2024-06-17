6e2ac9f1766811a58b8be02163a716f8d5261144
Abdulmalik <ayodejiamzat@gmail.com>
2024-06-17 15:01:23 +0100

    auto-commit

--- src/Components/DashBody.jsx
+++ src/Components/DashBody.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useReducer, useRef, useState } from 'react'
-import card from 
+import card from "../assets/card.png"
 const drops = {
     dashboard: false,
     cms: false,
