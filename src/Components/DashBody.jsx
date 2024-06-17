1fa67bdeb9df9238bb87aababe711d23dba73231
Abdulmalik <ayodejiamzat@gmail.com>
2024-06-17 15:09:45 +0100

    auto-commit

--- src/Components/DashBody.jsx
+++ src/Components/DashBody.jsx
@@ -91,7 +91,7 @@ const DashBody = (prop) => {
                             <h1 className='dark:text-white text-2xl font-semibold'>Dashboard</h1>
                             <div className='w-full'>
                                 <div className='md:w-1/2 w-full'>
-                                    <div>
+                                    <div className=''>
                                         <div className='w-2/3 cards'>
                                             kkkkkkkkk
                                         </div>
