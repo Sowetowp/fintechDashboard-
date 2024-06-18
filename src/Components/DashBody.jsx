69f683a8a84427d1ff8fd44c748f4fce247a2167
Abdulmalik <ayodejiamzat@gmail.com>
2024-06-18 22:10:01 +0100

    auto-commit

--- src/Components/DashBody.jsx
+++ src/Components/DashBody.jsx
@@ -105,14 +105,14 @@ const DashBody = (prop) => {
         // Fill the area under the curve
         ctx.lineTo(points[points.length - 1].x, chartHeight - padding); // Line down to the x-axis
         ctx.lineTo(points[0].x, chartHeight - padding); // Line back to the starting point on the x-axis
-        // ctx.closePath();
+        ctx.closePath();
         ctx.fillStyle = 'rgba(75, 192, 192, 0.2)';
         ctx.fill();
 
         // Draw the curve outline
         ctx.strokeStyle = 'rgba(75, 192, 192, 1)';
         ctx.lineWidth = 2;
-        // ctx.stroke();
+        ctx.stroke();
 
         // Draw points
         ctx.fillStyle = 'rgba(75, 192, 192, 1)';
