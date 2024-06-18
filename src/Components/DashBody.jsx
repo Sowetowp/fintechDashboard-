1650395ad4fc0fdc9f92290d55b68781453689ef
Abdulmalik <ayodejiamzat@gmail.com>
2024-06-18 13:09:50 +0100

    auto-commit

--- src/Components/DashBody.jsx
+++ src/Components/DashBody.jsx
@@ -119,7 +119,7 @@ const DashBody = (prop) => {
                                         <div className='w-[60%] bg-white m-auto shadow rounded-b-xl h-8'></div>
                                     </div>
                                     <div className='md:w-1/3 w-full bg-[#d7edfd] text-center'>
-                                        <p className='text'>Limit</p>
+                                        <p className='text-md'>Limit</p>
                                         <p>$4,000</p>
                                         <p>/$10,000</p>
                                     </div>
