2708615969fca45afd14459f86e8d0d983c529e1
Abdulmalik <ayodejiamzat@gmail.com>
2024-06-18 22:01:48 +0100

    auto-commit

--- src/Components/DashBody.jsx
+++ src/Components/DashBody.jsx
@@ -70,7 +70,7 @@ const DashBody = (prop) => {
         const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
 
         // Chart dimensions
-        const chartWidth = 900;
+        const chartWidth = 9000;
         const chartHeight = 400;
         const padding = 50;
 
