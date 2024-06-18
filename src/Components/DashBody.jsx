8f14256e41635fdb31acb8f6fae36e2a2b0580ee
Abdulmalik <ayodejiamzat@gmail.com>
2024-06-18 22:42:09 +0100

    auto-commit

--- src/Components/DashBody.jsx
+++ src/Components/DashBody.jsx
@@ -79,7 +79,7 @@ const DashBody = (prop) => {
         ctx.canvas.height = chartHeight;
 
         // Clear canvas
-        ctx.clearRect(0, 0, chartWidth, 1000);
+        ctx.clearRect(0, 0, chartWidth, 1000000);
 
         // Calculate points
         const xStep = (chartWidth - 2 * padding) / (data.length - 1);
