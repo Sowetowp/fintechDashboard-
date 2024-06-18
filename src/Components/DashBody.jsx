5b846b738ea75f25d86aeb9f62e00bbd9dd1be14
Abdulmalik <ayodejiamzat@gmail.com>
2024-06-18 22:06:25 +0100

    auto-commit

--- src/Components/DashBody.jsx
+++ src/Components/DashBody.jsx
@@ -99,7 +99,7 @@ const DashBody = (prop) => {
             const cp1y = points[i].y;
             const cp2x = points[i + 1].x - xStep / 2;
             const cp2y = points[i + 1].y;
-            ctx.bezierCurveTo(cp1x, cp1y, cp2x, cp2y, points[i + 1].x, points[i + 1].y);
+            // ctx.bezierCurveTo(cp1x, cp1y, cp2x, cp2y, points[i + 1].x, points[i + 1].y);
         }
 
         // Fill the area under the curve
