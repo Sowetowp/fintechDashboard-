b7ba7c144a5cdd9b65a10254660805546d017f48
Abdulmalik <ayodejiamzat@gmail.com>
2024-06-18 12:48:34 +0100

    auto-commit

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Navbar from '../Components/Navbar'
 import DashBody from '../Components/DashBody'
 
