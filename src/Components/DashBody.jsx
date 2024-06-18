a732f9cb369a0b09bb252ae25428cc4ce3fe3da3
Abdulmalik <ayodejiamzat@gmail.com>
2024-06-18 20:35:30 +0100

    auto-commit

--- src/Components/DashBody.jsx
+++ src/Components/DashBody.jsx
@@ -123,6 +123,10 @@ const DashBody = (prop) => {
                                         <p className=''>Limit</p>
                                         <p className='font-bold text-xl mt-1'>$4,000</p>
                                         <p className='text-slate-500'>/$10,000</p>
+                                        <div>
+                                            <h1>Curved Line Chart</h1>
+                                            <canvas ref={canvasRef}></canvas>
+                                        </div>
                                     </div>
                                 </div>
                                 <div className=' bg-slate-500 h-2 w-1/2'></div>
