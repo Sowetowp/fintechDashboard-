9203fae826c646039363ec84d6018c592a75c0e5
Abdulmalik <ayodejiamzat@gmail.com>
2024-06-22 19:51:51 +0100

    auto-commit

--- src/Components/DashBody.jsx
+++ src/Components/DashBody.jsx
@@ -107,7 +107,7 @@ const DashBody = (prop) => {
             const cp1x = points[i].x + xStep / 2;
             const cp1y = points[i].y -20;
             const cp2x = points[i + 1].x - xStep / 2;
-            const cp2y = points[i + 1].y;
+            const cp2y = points[i + 1].y -20;
             ctx.bezierCurveTo(cp1x, cp1y, cp2x, cp2y, points[i + 1].x, points[i + 1].y);
         }
 
