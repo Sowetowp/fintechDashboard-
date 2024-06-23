4d12a6026cff108af57e01a87c148bc760ded076
Abdulmalik <ayodejiamzat@gmail.com>
2024-06-23 15:06:14 +0100

    auto-commit

--- src/Components/DashBody.jsx
+++ src/Components/DashBody.jsx
@@ -239,7 +239,7 @@ const DashBody = (prop) => {
                                         </div>
                                     </div>
                                 </div>
-                                <div className=' bg-[#bfe1fa] h-2 w-1/2'></div>
+                                <div className=' bg-[#aedaf9] h-2 w-1/2'></div>
                             </div>
                         </div>
                     </div>
