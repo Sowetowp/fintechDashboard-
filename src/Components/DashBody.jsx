90a19ee6bf2192e466820a3f32f2ce8126e7cda0
Abdulmalik <ayodejiamzat@gmail.com>
2024-06-23 18:10:22 +0100

    auto-commit

--- src/Components/DashBody.jsx
+++ src/Components/DashBody.jsx
@@ -297,7 +297,7 @@ const DashBody = (prop) => {
                                                 <svg className='w-4' fill="grey" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g id="SVGRepo_iconCarrier"><path d="M11.178 19.569a.998.998 0 0 0 1.644 0l9-13A.999.999 0 0 0 21 5H3a1.002 1.002 0 0 0-.822 1.569l9 13z"></path></g></svg>
                                             </div>
                                         </div>
-                                        <div className='w-[80%] bg-white m-auto shadow rounded-b-xl h-8 relative'></div>
+                                        <div className='w-[80%] bg-white dark:bg-blue-950 m-auto shadow rounded-b-xl h-8 relative'></div>
                                         <div className='w-[60%] bg-white m-auto shadow rounded-b-xl h-8'></div>
                                     </div>
                                     <div className='md:w-1/3 pt-4 rounded-xl w-full bg-[#d7edfd] h-[fit-content] text-center overflow-hidden'>
