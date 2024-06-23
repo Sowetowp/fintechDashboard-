f9a3403840a7af1830568b4756cc331be224c69d
Abdulmalik <ayodejiamzat@gmail.com>
2024-06-23 14:44:18 +0100

    auto-commit

--- src/Components/DashBody.jsx
+++ src/Components/DashBody.jsx
@@ -136,8 +136,8 @@ const DashBody = (prop) => {
         }
 
         // Fill the area under the curve
-        ctx.lineTo(points[points.length - 1].x, chartHeight - padding - 250); // Line down to the x-axis
-        ctx.lineTo(points[0].x, chartHeight - padding  250); // Line back to the starting point on the x-axis
+        ctx.lineTo(points[points.length - 1].x, chartHeight - padding); // Line down to the x-axis
+        ctx.lineTo(points[0].x, chartHeight - padding); // Line back to the starting point on the x-axis
         ctx.closePath();
         ctx.fillStyle = 'green';
         ctx.fill();
