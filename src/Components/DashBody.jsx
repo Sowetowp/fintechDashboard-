916bbcae67c53e33d1f1bfab5a382e7ae3385818
Abdulmalik <ayodejiamzat@gmail.com>
2024-06-23 14:37:02 +0100

    auto-commit

--- src/Components/DashBody.jsx
+++ src/Components/DashBody.jsx
@@ -105,7 +105,7 @@ const DashBody = (prop) => {
         ctx.lineTo(points[points.length - 1].x, chartHeight - padding); // Line down to the x-axis
         ctx.lineTo(points[0].x, chartHeight - padding); // Line back to the starting point on the x-axis
         ctx.closePath();
-        ctx.fillStyle = 'red';
+        ctx.fillStyle = 'rgba(75, 192, 192, 0.2)';
         ctx.fill();
 
         // Draw curve and fill
@@ -122,7 +122,7 @@ const DashBody = (prop) => {
         ctx.lineTo(points[points.length - 1].x, chartHeight - padding); // Line down to the x-axis
         ctx.lineTo(points[0].x, chartHeight - padding); // Line back to the starting point on the x-axis
         ctx.closePath();
-        ctx.fillStyle = 'blue';
+        ctx.fillStyle = 'rgba(75, 192, 192, 0.2)';
         ctx.fill();
 
         // Draw curve and fill
@@ -139,7 +139,7 @@ const DashBody = (prop) => {
         ctx.lineTo(points[points.length - 1].x, chartHeight - padding); // Line down to the x-axis
         ctx.lineTo(points[0].x, chartHeight - padding); // Line back to the starting point on the x-axis
         ctx.closePath();
-        ctx.fillStyle = 'gree';
+        ctx.fillStyle = 'rgba(75, 192, 192, 0.2)';
         ctx.fill();
 
         // Draw the curve outline
