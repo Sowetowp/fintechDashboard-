8cd052bbabfd4576da051af051a132dad4ee04be
Abdulmalik <ayodejiamzat@gmail.com>
2024-06-23 18:28:17 +0100

    auto-commit

--- src/Components/DashBody.jsx
+++ src/Components/DashBody.jsx
@@ -310,7 +310,7 @@ const DashBody = (prop) => {
                                             </div>
                                         </div>
                                     </div>
-                                    <div className='bg-white dark:bg-blue-950 mt-12 shadow p-5'>
+                                    <div className='bg-white dark:bg-blue-950 mt-12 shadow p-6 rounded-lg'>
                                         <h2 className='dark:text-white'>Transaction Overview</h2>
                                     </div>
                                 </div>
