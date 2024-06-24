29963ea9e526a3167dc1573fd842fbd18e462fcd
Abdulmalik <ayodejiamzat@gmail.com>
2024-06-24 20:35:34 +0100

    auto-commit

--- src/Components/DashBody.jsx
+++ src/Components/DashBody.jsx
@@ -314,7 +314,7 @@ const DashBody = (prop) => {
                                         <h2 className='dark:text-white text-xl'>Transaction Overview</h2>
                                         <p className='text-gray-500 text-xs'>Lorem ipsum dolor sit amet, consectetur</p>
                                         <div>
-                                            <button className='dark:text-white bg-slate-100 flex dark:bg-[rgba(23,37,84,0.68)]'>
+                                            <button className='dark:text-white bg-slate-100 flex dark:bg-[rgb(23,37,84)]'>
                                                 <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g id="SVGRepo_iconCarrier"> <g id="Interface / Download"> <path id="Vector" d="M6 21H18M12 3V17M12 17L17 12M12 17L7 12" stroke="#0099FF" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"></path> </g> </g></svg>
                                                 Download Report
                                             </button>
