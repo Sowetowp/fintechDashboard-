3542834fa9ee68ad461a543fa5228cde97309a97
Abdulmalik <ayodejiamzat@gmail.com>
2024-06-24 23:31:33 +0100

    auto-commit

--- src/Components/DashBody.jsx
+++ src/Components/DashBody.jsx
@@ -336,7 +336,7 @@ const DashBody = (prop) => {
                                             ]}
                                             // width={500}
                                             // height={300}
-                                            className='w-full h-full'
+                                            className='w-full h-96'
                                         />
                                     </div>
                                 </div>
