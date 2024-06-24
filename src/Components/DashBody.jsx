4ddf275d24b979d0a2c21ac65058dd30c2ec7572
Abdulmalik <ayodejiamzat@gmail.com>
2024-06-24 22:24:10 +0100

    auto-commit

--- src/Components/DashBody.jsx
+++ src/Components/DashBody.jsx
@@ -36,47 +36,6 @@ const DashBody = (prop) => {
     const [height, setHeight] = useState("0px")
     const contentRef = useRef()
     const canvasRef = useRef(null);
-    const canvasRef2 = useRef(null);
-    const data = [10, 20, 30, 15, 25, 35, 45];
-
-    useEffect(() => {
-      const canvas = canvasRef.current;
-      const ctx = canvas.getContext('2d');
-      const chartWidth = canvas.width;
-      const chartHeight = canvas.height;
-  
-      // Chart dimensions
-      const padding = 50;
-      const barWidth = (chartWidth - 2 * padding) / data.length;
-      const maxValue = Math.max(...data);
-      const scaleFactor = (chartHeight - 2 * padding) / maxValue;
-  
-      // Clear the canvas
-      ctx.clearRect(0, 0, chartWidth, chartHeight);
-  
-      // Draw the bars
-      data.forEach((value, index) => {
-        const barHeight = value * scaleFactor;
-        const x = padding + index * barWidth;
-        const y = chartHeight - padding - barHeight;
-  
-        ctx.fillStyle = '#3498db';
-        ctx.fillRect(x, y, barWidth - 10, barHeight);
-  
-        // Add labels
-        ctx.fillStyle = '#000';
-        ctx.textAlign = 'center';
-        ctx.fillText(value, x + barWidth / 2 - 5, y - 5);
-      });
-  
-      // Draw axes
-      ctx.strokeStyle = '#000';
-      ctx.beginPath();
-      ctx.moveTo(padding, chartHeight - padding);
-      ctx.lineTo(padding, padding);
-      ctx.lineTo(chartWidth - padding, chartHeight - padding);
-      ctx.stroke();
-    }, [data]);
 
     useEffect(() => {
         if (contentRef.current) {
@@ -361,7 +320,6 @@ const DashBody = (prop) => {
                                             </button>
                                             <svg className='w-6' viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M7 12C7 13.1046 6.10457 14 5 14C3.89543 14 3 13.1046 3 12C3 10.8954 3.89543 10 5 10C6.10457 10 7 10.8954 7 12Z" className='fill-black dark:fill-white'></path> <path d="M14 12C14 13.1046 13.1046 14 12 14C10.8954 14 10 13.1046 10 12C10 10.8954 10.8954 10 12 10C13.1046 10 14 10.8954 14 12Z" className='fill-black dark:fill-white'></path> <path d="M21 12C21 13.1046 20.1046 14 19 14C17.8954 14 17 13.1046 17 12C17 10.8954 17.8954 10 19 10C20.1046 10 21 10.8954 21 12Z" className='fill-black dark:fill-white'></path> </g></svg>
                                         </div>
-                                        <canvas ref={canvasRef2} className='w-full h-96'></canvas>;
                                     </div>
                                 </div>
                                 <div className=' bg-[#37a9fb] h-2 w-1/2'></div>
