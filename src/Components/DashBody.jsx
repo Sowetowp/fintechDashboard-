dd1919d016cf971458fe53450132219971591d20
Abdulmalik <ayodejiamzat@gmail.com>
2024-06-25 23:02:07 +0100

    auto-commit

--- src/Components/DashBody.jsx
+++ src/Components/DashBody.jsx
@@ -328,7 +328,7 @@ const DashBody = (prop) => {
             id: 'barCategories',
             data: ['bar A', 'bar B', 'bar C'],
             scaleType: 'band',
-            width: "2", // Adjust this value to reduce the width of the bars
+            width: "px", // Adjust this value to reduce the width of the bars
         },
     ]}
     series={[
