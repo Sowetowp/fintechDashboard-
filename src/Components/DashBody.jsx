07001e8495d3288c1b3a33b58bb9942ca368ce2f
Abdulmalik <ayodejiamzat@gmail.com>
2024-06-27 01:42:36 +0100

    auto-commit

--- src/Components/DashBody.jsx
+++ src/Components/DashBody.jsx
@@ -324,9 +324,9 @@ const DashBody = (prop) => {
                                         <BarChart
                                             series={[
                                                 { data: [35, 44, 24, 34] },
-                                                // { data: [51, 6, 49, 30] },
-                                                // { data: [15, 25, 30, 50] },
-                                                // { data: [60, 50, 15, 25] },
+                                                { data: [51, 6, 49, 30] },
+                                                { data: [15, 25, 30, 50] },
+                                                { data: [60, 50, 15, 25] },
                                             ]}
                                             height={290}
                                             xAxis={[{ data: ['Q1', 'Q2', 'Q3', 'Q4'], scaleType: 'band' }]}
