2dd48969f3f049102b18d6fb38048e653e6e3ed9
Abdulmalik <ayodejiamzat@gmail.com>
2024-06-27 22:39:35 +0100

    auto-commit

--- src/Components/DashBody.jsx
+++ src/Components/DashBody.jsx
@@ -418,7 +418,7 @@ const DashBody = (prop) => {
                                                 <p className='text-gray-500 text-sm'>Income</p>
                                                 <p className='dark:text-white text-2xl font-bold mt-1'>$65,123</p>
                                                 <p className='text-gray-500 text-xs mt-1 flex gap-3 items-center'>
-                                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M7 24h-6v-6h6v6zm8-9h-6v9h6v-9zm8-4h-6v13h6v-13zm0-11l-6 1.221 1.716 1.708-6.85 6.733-3.001-3.002-7.841 7.797 1.41 1.418 6.427-6.39 2.991 2.993 8.28-8.137 1.667 1.66 1.201-6.001z" /></svg>
+                                                    <svg className='w-4' xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M7 24h-6v-6h6v6zm8-9h-6v9h6v-9zm8-4h-6v13h6v-13zm0-11l-6 1.221 1.716 1.708-6.85 6.733-3.001-3.002-7.841 7.797 1.41 1.418 6.427-6.39 2.991 2.993 8.28-8.137 1.667 1.66 1.201-6.001z" /></svg>
                                                     {"4% (30 days)"}
                                                 </p>
                                             </div>
