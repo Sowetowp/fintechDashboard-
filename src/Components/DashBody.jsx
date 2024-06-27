3f4dfdd6c2445015f78a17cfce45a07dac0eef3b
Abdulmalik <ayodejiamzat@gmail.com>
2024-06-27 01:53:49 +0100

    auto-commit

--- src/Components/DashBody.jsx
+++ src/Components/DashBody.jsx
@@ -330,7 +330,7 @@ const DashBody = (prop) => {
                                             ]}
                                             height={290}
                                             xAxis={[{ data: ['Q1', 'Q2', 'Q3', 'Q4'], scaleType: 'band' }]}
-                                            margin={{ top: 10, bottom: 30, left: 40, right: 10 }}
+                                            margin={{ top: 100, bottom: 30, left: 40, right: 10 }}
                                         />
                                     </div>
                                 </div>
