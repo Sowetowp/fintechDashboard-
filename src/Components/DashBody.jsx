9c5d7c27fe06142c83fde1fb73e6879b64c282a9
Abdulmalik <ayodejiamzat@gmail.com>
2024-06-27 16:50:57 +0100

    auto-commit

--- src/Components/DashBody.jsx
+++ src/Components/DashBody.jsx
@@ -104,6 +104,26 @@ const DashBody = (prop) => {
         <svg className={`w-6 ${prop.prop ? "" : "hidden md:block"}`} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M7 2L16.5 2L21 6.5V19" className="group-hover:stroke-[#0099FF] dark:group-hover:stroke-[white] stroke-[rgb(107,114,128)] dark:stroke-[rgb(209,213,219)]" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"></path> <path d="M3 20.5V6.5C3 5.67157 3.67157 5 4.5 5H14.2515C14.4106 5 14.5632 5.06321 14.6757 5.17574L17.8243 8.32426C17.9368 8.43679 18 8.5894 18 8.74853V20.5C18 21.3284 17.3284 22 16.5 22H4.5C3.67157 22 3 21.3284 3 20.5Z" className="group-hover:stroke-[#0099FF] dark:group-hover:stroke-[white] stroke-[rgb(107,114,128)] dark:stroke-[rgb(209,213,219)]" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"></path> <path d="M14 8.4V5.35355C14 5.15829 14.1583 5 14.3536 5C14.4473 5 14.5372 5.03725 14.6036 5.10355L17.8964 8.39645C17.9628 8.46275 18 8.55268 18 8.64645C18 8.84171 17.8417 9 17.6464 9H14.6C14.2686 9 14 8.73137 14 8.4Z" className="group-hover:stroke-[#0099FF] dark:group-hover:stroke-[white] stroke-[rgb(107,114,128)] dark:stroke-[rgb(209,213,219)]" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"></path> </g></svg>
     ]
 
+    const scrollRef = useRef(null);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (scrollRef.current) {
+        const { scrollLeft, scrollWidth, clientWidth } = scrollRef.current;
+        if (scrollLeft + clientWidth >= scrollWidth - 10) {
+          setItems((prevItems) => [...prevItems, ...data]);
+        } else if (scrollLeft === 0) {
+          setItems((prevItems) => [...data, ...prevItems]);
+          scrollRef.current.scrollLeft = scrollWidth;
+        }
+      }
+    };
+
+    const ref = scrollRef.current;
+    ref.addEventListener('scroll', handleScroll);
+    return () => ref.removeEventListener('scroll', handleScroll);
+  }, [data]);
+  
     useEffect(() => {
         const canvas = canvasRef.current;
         const ctx = canvas.getContext('2d');
