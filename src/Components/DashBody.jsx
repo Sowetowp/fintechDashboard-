80b1e402674034ee2ff271f08999d08faeea342b
Abdulmalik <ayodejiamzat@gmail.com>
2024-06-27 18:01:32 +0100

    auto-commit

--- src/Components/DashBody.jsx
+++ src/Components/DashBody.jsx
@@ -5,6 +5,9 @@ import notif1 from "../assets/1.jpg"
 import notif2 from "../assets/2.jpg"
 import notif3 from "../assets/3.jpg"
 import notif4 from "../assets/4.jpg"
+import Flickity from 'flickity';
+import 'flickity/css/flickity.css';
+import './InfiniteScroll.css'; // Your custom styles
 
 const drops = {
     dashboard: false,
