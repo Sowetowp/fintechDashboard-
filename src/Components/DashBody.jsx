790610b86e7f6821b7784bc36a0407dc6c5219b5
Abdulmalik <ayodejiamzat@gmail.com>
2024-06-27 03:05:07 +0100

    auto-commit

--- src/Components/DashBody.jsx
+++ src/Components/DashBody.jsx
@@ -330,7 +330,7 @@ const DashBody = (prop) => {
                                                 strokeWidth:"0.5"
                                             })}
                                             series={[
-                                                { data: [35, 44, 24, 34, 15, 25, 80], id: 'run', dataKey: 'month' },
+                                                { data: [35, 44, 24, 34, 15, 25, 80], id: 'run' },
                                                 { data: [51, 6, 49, 30, 60, 50, 15], id: 'cyc' }
                                             ]}
                                             height={290}
