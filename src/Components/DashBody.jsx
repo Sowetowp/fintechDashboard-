576b56e6bb7c8586eb67ade4e69f4ea17c6084be
Abdulmalik <ayodejiamzat@gmail.com>
2024-06-27 23:28:41 +0100

    auto-commit

--- src/Components/DashBody.jsx
+++ src/Components/DashBody.jsx
@@ -491,7 +491,7 @@ const DashBody = (prop) => {
                                         </div>
                                         <LineChart
                                             height={300}
-                                            series={[{ data: [4000, 3000, 2000, 2780, 1890, 2390, 3490], label: 'uv', area: true, showMark: false }]}
+                                            series={[{ data: [4000, 3000, 2000, 2780, 1890, 2390, 3490], area: true, showMark: false }]}
                                             xAxis={[{ scaleType: 'point', data: ['Page A', 'Page B', 'Page C', 'Page D', 'Page E', 'Page F', 'Page G'] }]}
                                             sx={{
                                                 [`& .${lineElementClasses.root}`]: {
