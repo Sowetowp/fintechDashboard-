9883d2f59c57aa2ff721701234ca8e6f31451213
Abdulmalik <ayodejiamzat@gmail.com>
2024-06-27 20:55:20 +0100

    auto-commit

--- src/Components/DashBody.jsx
+++ src/Components/DashBody.jsx
@@ -422,7 +422,7 @@ const DashBody = (prop) => {
                                             xAxis={[{ data: [0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24], disableLine: true, disableTicks: true }]}
                                             series={[
                                                 {
-                                                    data: [0, 4, 2.5, 2, 5, 1.5, 5], 
+                                                    data: [0, 4, 2.5, 6, 3, 1.5, 5], 
                                                     showMark: false
                                                 },
                                             ]}
