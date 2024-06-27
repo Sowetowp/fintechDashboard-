6e1d0d7af7df9f3c68680b6e9c0f0a57fff7b1c4
Abdulmalik <ayodejiamzat@gmail.com>
2024-06-27 03:26:58 +0100

    auto-commit

--- src/Components/DashBody.jsx
+++ src/Components/DashBody.jsx
@@ -334,6 +334,7 @@ const DashBody = (prop) => {
                                                 { data: [51, 6, 49, 30, 60, 50, 15], id: 'cyc' }
                                             ]}
                                             height={290}
+                                            axisHighlight={"none"}
                                             grid={{ horizontal: true }}
                                             borderRadius={50}
                                             colors={['rgb(225, 225, 225)', 'rgb(239, 239, 239)']}
