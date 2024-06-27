98cfbd97c6529a8195758bb17ea856bae0639ac7
Abdulmalik <ayodejiamzat@gmail.com>
2024-06-27 02:37:12 +0100

    auto-commit

--- src/Components/DashBody.jsx
+++ src/Components/DashBody.jsx
@@ -324,7 +324,7 @@ const DashBody = (prop) => {
                                         <BarChart
                                             sx={(theme) => ({
                                                 [`.${barElementClasses.root}`]: {
-                                                    color: "red",
+                                                    backgroundColor: "red",
                                                   },
                                                 stroke:"grey",
                                                 strokeWidth:"0.5"
