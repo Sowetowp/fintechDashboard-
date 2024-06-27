f55790e5f837daa1e881ab429aee1086b2204ded
Abdulmalik <ayodejiamzat@gmail.com>
2024-06-27 20:46:00 +0100

    auto-commit

--- src/Components/DashBody.jsx
+++ src/Components/DashBody.jsx
@@ -427,7 +427,7 @@ const DashBody = (prop) => {
                                                 },
                                             ]}
                                             height={200}
-                                            yAxis={[{ disableLine: true, disableTicks: true }]}
+                                            yAxis={[{ disableLine: true }]}
                                             margin={{ left: 0, right: 0, top: 0, bottom: 0 }}
                                         />
                                     </div>
