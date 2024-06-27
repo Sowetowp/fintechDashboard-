c909eda381786766f9d7c909e0be30f4e093aee3
Abdulmalik <ayodejiamzat@gmail.com>
2024-06-27 21:34:41 +0100

    auto-commit

--- src/Components/DashBody.jsx
+++ src/Components/DashBody.jsx
@@ -435,9 +435,8 @@ const DashBody = (prop) => {
                                                 strokeWidth: 0,
                                             }}
                                             colors={["#0099FF"]}
-                                            padding={{bottom: 90}}
                                             yAxis={[{ disableLine: true, disableTicks: true }]}
-                                            margin={{ left: 0, right: 0, top: 0, bottom: 20 }}
+                                            margin={{ left: 0, right: 0, top: 0, bottom: 0 }}
                                         />
                                     </div>
                                 </div>
