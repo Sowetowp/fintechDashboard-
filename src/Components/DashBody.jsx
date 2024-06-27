e37f7ff8337be99576bcb5cb76799ac98e8be936
Abdulmalik <ayodejiamzat@gmail.com>
2024-06-27 20:08:44 +0100

    auto-commit

--- src/Components/DashBody.jsx
+++ src/Components/DashBody.jsx
@@ -414,7 +414,7 @@ const DashBody = (prop) => {
                                 <div className=' bg-[#37a9fb] h-2 w-1/2'></div>
                             </div>
                             <div className='w-full text-center pt-12'>
-                                <p className='dark:text-white'>Copyright © Developed by <span className=''>Abdulmalik</span> 2023</p>
+                                <p className='dark:text-white'>Copyright © Developed by <span className='text-[#0099FF] hover:text-[#0099FF]'>Abdulmalik</span> 2023</p>
                             </div>
                         </div>
                     </div>
