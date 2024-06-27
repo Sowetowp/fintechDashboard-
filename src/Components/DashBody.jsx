54a6964411f5d172021d23205779eea930f219cf
Abdulmalik <ayodejiamzat@gmail.com>
2024-06-27 23:22:06 +0100

    auto-commit

--- src/Components/DashBody.jsx
+++ src/Components/DashBody.jsx
@@ -502,8 +502,7 @@ const DashBody = (prop) => {
                                                 },
                                             ]}
                                             xAxis={[{
-                                                scaleType: 'point', data: ['Page A', 'Page B', 'Page C',
-                                                    'Page D', 'Page E', 'Page F', 'Page G']
+                                                scaleType: 'point', data: ['Page A', 'Page B', 'Page C', 'Page D', 'Page E', 'Page F', 'Page G']
                                             }]}
                                         >
                                             <AreaPlot />
