485e67ac5d488097d78dd0ad0aa8dd4831a2a196
Abdulmalik <ayodejiamzat@gmail.com>
2024-06-27 14:26:00 +0100

    auto-commit

--- src/Components/DashBody.jsx
+++ src/Components/DashBody.jsx
@@ -354,7 +354,7 @@ const DashBody = (prop) => {
                                         </div>
                                         <div>
                                             <div className='w-[fit-content] bg-[#0099ff2a] py-3 px-1 rounded-xl'>
-                                                <img src={notif1} alt="" className='w-20 rounded-xl'/>
+                                                <img src={notif1} alt="" className='w-22 rounded-xl'/>
                                                 <p className='dark:text-white text-lg text-center mt-3'>Samuel</p>
                                                 <p className='text-gray-500 text-xs text-center mt-1'>@sam224</p>
                                             </div>
