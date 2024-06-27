ca996ec88870b09eb9e07642eef068e76869f75b
Abdulmalik <ayodejiamzat@gmail.com>
2024-06-27 02:40:00 +0100

    auto-commit

--- src/Components/DashBody.jsx
+++ src/Components/DashBody.jsx
@@ -323,7 +323,7 @@ const DashBody = (prop) => {
                                         </div>
                                         <BarChart
                                             sx={(theme) => ({
-                                                [`.${barElementClasses.root}`]: {
+                                                [`.MuiBarElement-series-l_id`]: {
                                                     borderRadius: "100px",
                                                   },
                                                 stroke:"grey",
