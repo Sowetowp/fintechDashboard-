f3d9b94501b0719fc297ab07c77946b4d5ab38d9
Abdulmalik <ayodejiamzat@gmail.com>
2024-06-27 03:15:22 +0100

    auto-commit

--- src/Components/DashBody.jsx
+++ src/Components/DashBody.jsx
@@ -334,7 +334,7 @@ const DashBody = (prop) => {
                                             grid={{ horizontal: true }}
                                             borderRadius={50}
                                             colors={['rgb(225, 225, 225)', 'rgb(239, 239, 239)']}
-                                            xAxis={[{ data: ['Sun', 'Mon', 'Tue', 'Wed', "Thu", "Fri", "Sat"], scaleType: 'band' }]}
+                                            xAxis={[{ data: ['Sun', 'Mon', 'Tue', 'Wed', "Thu", "Fri", "Sat"],data: xLabels , scaleType: 'band' }]}
                                             margin={{ top: 10, bottom: 30, left: 40, right: 10 }}
                                         />
                                     </div>
