24c52b33bb5a211ec74ca48bd890a82418436f60
Abdulmalik <ayodejiamzat@gmail.com>
2024-06-27 22:24:18 +0100

    auto-commit

--- src/Components/DashBody.jsx
+++ src/Components/DashBody.jsx
@@ -411,7 +411,7 @@ const DashBody = (prop) => {
                                         </div>
                                     </div>
                                 </div>
-                                <div className='lg:w-1/2 w-full'>
+                                <div className='lg:w-1/2 w-full '>
                                     <div className='dark:bg-blue-950 md:w-1/2 w-full rounded-xl pb-3'>
                                         <div className='pt-7 px-7 pb-2'>
                                             <p className='text-gray-500 text-sm'>Income</p>
