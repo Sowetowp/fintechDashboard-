c139ce19bada5617edaab684cda25add1d5a021a
Abdulmalik <ayodejiamzat@gmail.com>
2024-06-27 02:24:34 +0100

    auto-commit

--- src/Components/DashBody.jsx
+++ src/Components/DashBody.jsx
@@ -331,7 +331,7 @@ const DashBody = (prop) => {
                                                 { data: [51, 6, 49, 30, 60, 50, 15] }
                                             ]}
                                             height={290}
-                                            borderRadius={["100px", "10px"]}
+                                            borderRadius={[10,10]}
                                             colors={['yellow', '#EC407A']}
                                             xAxis={[{ data: ['Sun', 'Mon', 'Tue', 'Wed', "Thu", "Fri", "Sat"], scaleType: 'band' }]}
                                             margin={{ top: 10, bottom: 30, left: 40, right: 10 }}
