3090466a56e2e2c087967223c01b1a4814e413f0
Abdulmalik <ayodejiamzat@gmail.com>
2024-06-27 21:04:04 +0100

    auto-commit

--- src/Components/DashBody.jsx
+++ src/Components/DashBody.jsx
@@ -427,7 +427,7 @@ const DashBody = (prop) => {
                                                 },
                                             ]}
                                             height={90}
-                                            width={"100%"}
+                                            width={"100"}
                                             sx={{
                                                 [`& .${lineElementClasses.root}`]: {
                                                   stroke: "red",
