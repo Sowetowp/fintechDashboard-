1cad950b45dfbceaa3a2d67907b52a9e19e5ea02
Abdulmalik <ayodejiamzat@gmail.com>
2024-06-27 19:55:23 +0100

    auto-commit

--- src/Components/DashBody.jsx
+++ src/Components/DashBody.jsx
@@ -393,7 +393,7 @@ const DashBody = (prop) => {
                                             </span>
                                             <p className='dark:text-white text-xl font-bold'>$56,772.38</p>
                                         </div>
-                                        <div className='mt-10 snap-x flex gap-3 overflow-x-scroll whitespace-nowrap scroll-smooth scrooler'>
+                                        <div className='mt-10 snap-x flex gap-2 overflow-x-scroll whitespace-nowrap scroll-smooth scrooler'>
                                             {quickTransfer.map((qt, index) => (
                                                 <div key={index} className={`w-[fit-content] scroll-ml-14 snap-start hover:bg-[#0099ff2a] py-3 px-1 rounded-xl`}>
                                                     <img src={qt.image} alt="" className='md:min-w-[5.5rem] w-full  rounded-xl' />
