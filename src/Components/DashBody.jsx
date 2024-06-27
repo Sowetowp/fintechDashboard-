dffa8b64070ddea9e29cad64fe71566026227d89
Abdulmalik <ayodejiamzat@gmail.com>
2024-06-27 16:34:17 +0100

    auto-commit

--- src/Components/DashBody.jsx
+++ src/Components/DashBody.jsx
@@ -405,7 +405,7 @@ const DashBody = (prop) => {
                                         <div className='mt-10 flex justify-between items-center'>
                                             <p className='dark:text-white text-xl font-medium'>Amount</p>
                                             <div className='flex items-center bg-gray-500'>
-                                                <input type="text" className='outline-0'/>
+                                                <input type="text" className='outline-0 bg-transparent'/>
                                                 <button>TRANSFER NOW</button>
                                             </div>
                                         </div>
