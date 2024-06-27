d5b1c249fd5bfa976655b5cbff315f759a12bfbb
Abdulmalik <ayodejiamzat@gmail.com>
2024-06-27 16:51:40 +0100

    auto-commit

--- src/Components/DashBody.jsx
+++ src/Components/DashBody.jsx
@@ -123,7 +123,7 @@ const DashBody = (prop) => {
     ref.addEventListener('scroll', handleScroll);
     return () => ref.removeEventListener('scroll', handleScroll);
   }, [data]);
-  
+
     useEffect(() => {
         const canvas = canvasRef.current;
         const ctx = canvas.getContext('2d');
@@ -413,7 +413,7 @@ const DashBody = (prop) => {
                                             </span>
                                             <p className='dark:text-white text-xl font-bold'>$56,772.38</p>
                                         </div>
-                                        <div className='mt-10 snap-x flex gap-5 overflow-x-scroll whitespace-nowrap scroll-smooth scrooler'>
+                                        <div ref={scrollRef} className='mt-10 snap-x flex gap-5 overflow-x-scroll whitespace-nowrap scroll-smooth scrooler'>
                                             {quickTransfer.map((qt, index) => (
                                                 <div key={index} className='w-[fit-content] scroll-ml-14 snap-start bg-[#0099ff2a] py-3 px-1 rounded-xl'>
                                                     <img src={qt.image} alt="" className='min-w-20 rounded-xl' />
