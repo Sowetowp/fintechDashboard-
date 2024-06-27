58ca8227b551c3343917cd7d1992a4c889692ad2
Abdulmalik <ayodejiamzat@gmail.com>
2024-06-27 17:02:04 +0100

    auto-commit

--- src/Components/DashBody.jsx
+++ src/Components/DashBody.jsx
@@ -426,7 +426,7 @@ const DashBody = (prop) => {
                                             <p className='dark:text-white text-xl font-medium'>Amount</p>
                                             <div className='flex items-center bg-gray-100 rounded-xl'>
                                                 <input type="text" className='outline-0 bg-transparent px-4 md:w-36 w-1/2'/>
-                                                <button className='bg-[#0099FF] py-3 px-4 text-xs font-medium text-white rounded-xl'>TRANSFER NOW</button>
+                                                <button className='bg-[#0099FF] py-3 px-4 text-xs font-medium text-white rounded-xl mr-auto'>TRANSFER NOW</button>
                                             </div>
                                         </div>
                                     </div>
