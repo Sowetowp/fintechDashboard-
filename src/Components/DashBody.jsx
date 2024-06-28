4145eced678d6952fc8db879339465e11f728ec1
Abdulmalik <ayodejiamzat@gmail.com>
2024-06-28 01:27:10 +0100

    auto-commit

--- src/Components/DashBody.jsx
+++ src/Components/DashBody.jsx
@@ -554,7 +554,7 @@ const DashBody = (prop) => {
                                                     sx={{
                                                         
                                                       }}
-                                                      colors={["red, "blue"]}
+                                                      colors={["red", "blue"]}
                                                     margin={{ left: 0, right: 0, top: 0, bottom: 0 }}
                                                 />
                                             </div>
