8d6246c25d0bcc3daa5434c08cdc3978de603527
Abdulmalik <ayodejiamzat@gmail.com>
2024-06-28 01:35:20 +0100

    auto-commit

--- src/Components/DashBody.jsx
+++ src/Components/DashBody.jsx
@@ -502,6 +502,7 @@ const DashBody = (prop) => {
                                                 },
                                               }}
                                             colors={["#199dc9"]}
+                                            border={}
                                             margin={{ left: 0, right: 0, top: 0, bottom: 0 }}
                                         />
                                     </div>
