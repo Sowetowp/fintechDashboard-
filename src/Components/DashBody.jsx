d69f216efa21f23fdbe65b22b6d6d37b0b25e76f
Abdulmalik <ayodejiamzat@gmail.com>
2024-06-28 01:17:10 +0100

    auto-commit

--- src/Components/DashBody.jsx
+++ src/Components/DashBody.jsx
@@ -545,9 +545,9 @@ const DashBody = (prop) => {
                                                     series={[
                                                         {
                                                             data: [
-                                                                { id: 0, value: 10, label: 'series A' },
-                                                                { id: 1, value: 15, label: 'series B' },
-                                                                { id: 2, value: 20, label: 'series C' },
+                                                                { id: 0, value: 10 },
+                                                                { id: 1, value: 15 },
+                                                                { id: 2, value: 20 },
                                                             ],
                                                         },
                                                     ]}
