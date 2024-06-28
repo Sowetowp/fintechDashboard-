d93984d23158168910f0c2538b8bb7c88ae5cc39
Abdulmalik <ayodejiamzat@gmail.com>
2024-06-28 01:25:31 +0100

    auto-commit

--- src/Components/DashBody.jsx
+++ src/Components/DashBody.jsx
@@ -560,6 +560,7 @@ const DashBody = (prop) => {
                                                         },
                                                         fill:"red"
                                                       }}
+                                                      disableLine={{true}}
                                                     margin={{ left: 0, right: 0, top: 0, bottom: 0 }}
                                                 />
                                             </div>
