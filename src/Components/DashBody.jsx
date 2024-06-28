60e17593479401faed9d69ec11924b91fe2b42df
Abdulmalik <ayodejiamzat@gmail.com>
2024-06-28 01:37:24 +0100

    auto-commit

--- src/Components/DashBody.jsx
+++ src/Components/DashBody.jsx
@@ -495,10 +495,11 @@ const DashBody = (prop) => {
                                             series={[{ data: [2, 4, 2.5, 6, 3, 4, 2, 4, 0.5, 5, 2, 9, 4.3, 5], area: true, showMark: false }]}
                                             xAxis={[{ scaleType: 'point', data: ['Page A', 'Page B', 'Page C', 'Page D', 'Page E', 'Page F', 'Page G', 'Page H', 'Page I', 'Page J', 'Page K', 'Page L', 'Page M', 'Page N'], disableLine: true, disableTicks: true }]}
                                             sx={{
-                                                [`& .${lineElementClasses.root}`]: {
-                                                    display: 'none',
-                                                },
-                                            }}
+        [`& .${pieArcLabelClasses.root}`]: {
+          fill: 'white',
+          fontWeight: 'bold',
+        },
+      }}
                                             colors={["#199dc9"]}
                                             margin={{ left: 0, right: 0, top: 0, bottom: 0 }}
                                         />
@@ -552,7 +553,7 @@ const DashBody = (prop) => {
                                                     ]}
                                                     height={70}
                                                     sx={{
-                                                        bor:""
+                                                        border:"none"
                                                     }}
                                                     colors={["white", "rgba(243,232,255,0.27)"]}
                                                     margin={{ left: 0, right: 0, top: 0, bottom: 0 }}
