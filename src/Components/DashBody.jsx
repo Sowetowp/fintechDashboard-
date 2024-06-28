181595352ffd24f83f6c54f9f9b73444ba0abbce
Abdulmalik <ayodejiamzat@gmail.com>
2024-06-28 01:20:19 +0100

    auto-commit

--- src/Components/DashBody.jsx
+++ src/Components/DashBody.jsx
@@ -552,8 +552,9 @@ const DashBody = (prop) => {
                                                     ]}
                                                     height={200}
                                                     sx={{
-                                                        
+
                                                     }}
+                                                    margin={{ left: 0, right: 0, top: 0, bottom: 0 }}
                                                 />
                                             </div>
                                         </div>
