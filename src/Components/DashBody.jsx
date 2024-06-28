724a79d2d4065da84c38fbf589b1cdc4ea16d03c
Abdulmalik <ayodejiamzat@gmail.com>
2024-06-28 02:28:10 +0100

    auto-commit

--- src/Components/DashBody.jsx
+++ src/Components/DashBody.jsx
@@ -583,9 +583,9 @@ const DashBody = (prop) => {
                                                 </div>
                                             </div>
                                             <div className='flex flex-wrap md:flex-nowrap mt-6 gap-3'>
-                                                <div className='md:w-1/2 w-full rounded-xl bg-purple-500 p-4'>
+                                                <div className='md:w-1/2 w-full rounded-xl bg-transparent p-4'>
                                                     <div className='flex justify-center items-center'>
-                                                        <div className='absolute z-10 rounded-full w-16 h-16 m-auto bg-purple-500 flex items-center justify-center font-medium text-white text-md'>5%</div>
+                                                        <div className='absolute z-10 rounded-full w-16 h-16 m-auto bg-transparent flex items-center justify-center font-medium text-white text-md'>5%</div>
                                                         <PieChart
                                                             series={[
                                                                 {
