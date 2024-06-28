5dffb5631f70ec8421b280b17ca03a79142c5ae0
Abdulmalik <ayodejiamzat@gmail.com>
2024-06-28 02:09:19 +0100

    auto-commit

--- src/Components/DashBody.jsx
+++ src/Components/DashBody.jsx
@@ -556,7 +556,7 @@ const DashBody = (prop) => {
                                                         margin={{ left: 0, right: 0, top: 0, bottom: 0 }}
                                                     />
                                                 </div>
-                                                <p className='text-white'>Investment</p>
+                                                <p className='text-white text-center mt-5'>Investment</p>
                                             </div>
                                         </div>
                                     </div>
