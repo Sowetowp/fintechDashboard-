9c8208b070418bf20298b55ada86aa5d5ac43a33
Abdulmalik <ayodejiamzat@gmail.com>
2024-06-29 17:31:50 +0100

    auto-commit

--- src/Components/DashBody.jsx
+++ src/Components/DashBody.jsx
@@ -649,7 +649,7 @@ const DashBody = (prop) => {
                                     </div>
                                     <div className='dark:bg-blue-950 bg-white shadow mt-7 p-7 w-full rounded-xl'>
                                         <div className='flex flex-wrap justify-between items-center'>
-                                            <div >
+                                            <div>
                                                 <h2 className='dark:text-white text-xl font-medium'>Previous Transactions</h2>
                                                 <p className='text-gray-500 text-xs'>Lorem ipsum dolor sit amet, consectetur</p>
                                             </div>
