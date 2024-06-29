98a1f6068acca2863b66a1add933e2e38ef8e912
Abdulmalik <ayodejiamzat@gmail.com>
2024-06-29 17:18:01 +0100

    auto-commit

--- src/Components/DashBody.jsx
+++ src/Components/DashBody.jsx
@@ -659,8 +659,8 @@ const DashBody = (prop) => {
                                                 <a onClick={() => setDay("today")} className={`rounded-full dark:text-[#0099FF] pb-2 pt-1 inline-block px-4 cursor-pointer ${day === "today" ? "bg-[#0099FF] text-white dark:text-white" : ""}`}>today</a>
                                             </div>
                                         </div>
-                                        <div className='w-full overflow-x-scroll'>
-                                            <table className='w-5'>
+                                        <div className='w-full px-2 overflow-x-auto'>
+                                            <table className='w-full'>
                                                 <tbody>
                                                     <tr>
                                                         <td className='py-4'>
