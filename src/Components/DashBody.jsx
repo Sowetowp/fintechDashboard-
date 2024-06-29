e7b76ee7886d5b62644898bc5dfd02067f3410e8
Abdulmalik <ayodejiamzat@gmail.com>
2024-06-29 11:03:50 +0100

    auto-commit

--- src/Components/DashBody.jsx
+++ src/Components/DashBody.jsx
@@ -648,7 +648,10 @@ const DashBody = (prop) => {
                                     </div>
                                     <div className='dark:bg-blue-950 bg-white shadow mt-7 p-7 w-full rounded-xl'>
                                         <div>
-                                            
+                                            <div>
+                                                
+                                            </div>
+                                            <div></div>
                                         </div>
                                     </div>
                                 </div>
