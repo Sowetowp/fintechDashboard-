24c444cc5de7894f6353561fe46f2d0d65e85eec
Abdulmalik <ayodejiamzat@gmail.com>
2024-06-29 11:08:53 +0100

    auto-commit

--- src/Components/DashBody.jsx
+++ src/Components/DashBody.jsx
@@ -653,7 +653,7 @@ const DashBody = (prop) => {
                                                 <p className='text-gray-500 text-xs'>Lorem ipsum dolor sit amet, consectetur</p>
                                             </div>
                                             <div className='rounded-full bg-gray-100'>
-                                                <a className='rounded-full'>monthly</a>
+                                                <a className='rounded-full p-3'>monthly</a>
                                                 <a className='rounded-full'>weekly</a>
                                                 <a className='rounded-full'>today</a>
                                             </div>
