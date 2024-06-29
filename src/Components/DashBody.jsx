bc76d1e526f5dbc23c9fbcb86da5984aa389d93a
Abdulmalik <ayodejiamzat@gmail.com>
2024-06-29 17:19:55 +0100

    auto-commit

--- src/Components/DashBody.jsx
+++ src/Components/DashBody.jsx
@@ -647,8 +647,8 @@ const DashBody = (prop) => {
                                             </div>
                                         </div>
                                     </div>
-                                    <div className='dark:bg-blue-950 bg-white shadow mt-7 py-7 w-full rounded-xl'>
-                                        <div className='flex flex-wrap px-7 md:flex-nowrap justify-between items-center'>
+                                    <div className='dark:bg-blue-950 bg-white shadow mt-7 p-7 w-full rounded-xl'>
+                                        <div className='flex flex-wrap md:flex-nowrap justify-between items-center'>
                                             <div>
                                                 <h2 className='dark:text-white text-xl font-medium'>Previous Transactions</h2>
                                                 <p className='text-gray-500 text-xs'>Lorem ipsum dolor sit amet, consectetur</p>
@@ -659,7 +659,7 @@ const DashBody = (prop) => {
                                                 <a onClick={() => setDay("today")} className={`rounded-full dark:text-[#0099FF] pb-2 pt-1 inline-block px-4 cursor-pointer ${day === "today" ? "bg-[#0099FF] text-white dark:text-white" : ""}`}>today</a>
                                             </div>
                                         </div>
-                                        <div className='w-full px-7 overflow-x-auto'>
+                                        <div className='w-full px-2 overflow-x-auto'>
                                             <table className='w-full'>
                                                 <tbody>
                                                     <tr>
